89cb45fc854a80345d96829a2c40290e378bef43
Vasu Sharma <vasu22300@gmail.com>
2025-03-22 23:33:14 +0530

    Miscellaneous changes made

--- Projects/3-todo-app-version-two/src/Components/TodoHeading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./TodoHeading.module.css";
-
-function TodoHeading() {
-  return <h1 className={style.heading}>TODO APP</h1>;
-}
-
-export default TodoHeading;
